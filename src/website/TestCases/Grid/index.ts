4e719058d75db9b2c5ddf28d88b9d30a2839e593
stereobooster <stereobooster@gmail.com>
2019-05-14 16:17:53 +0200

    Demo of the bug with wrong grouping

--- src/website/TestCases/Grid/index.ts
+++ src/website/TestCases/Grid/index.ts
@@ -3,5 +3,6 @@ import * as table3 from "./table-3";
 import * as table4 from "./table-4";
 import * as table5 from "./table-5";
 import * as table6 from "./table-6";
+import * as table7 from "./table-7";
 
-export default [table1, table3, table4, table5, table6];
+export default [table1, table3, table4, table5, table6, table7];
