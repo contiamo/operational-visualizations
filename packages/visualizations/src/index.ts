c8ce0affef15b62aa0991489748fa5159f970bda
stereobooster <stereobooster@gmail.com>
2019-05-22 18:45:23 +0200

    It builds, but it's broken

--- packages/visualizations/src/index.ts
+++ packages/visualizations/src/index.ts
@@ -1,23 +1,4 @@
-// Simple imports and exports for consumers of the library.
-export { default as Chart } from "./Chart/facade";
-export { default as PieChart } from "./PieChart/facade";
-export { default as ProcessFlow } from "./ProcessFlow/facade";
-export { default as ProcessFlowLoopHandler } from "./utils/process_flow_loop_handler";
-export { default as Sunburst } from "./Sunburst/facade";
-export { default as VisualizationWrapper } from "./utils/VisualizationWrapper";
-export { default as Legend } from "./Legend/Legend";
-export { default as Axis } from "./Axis/Axis";
-export { colorAssigner } from "./utils/colorAssigner";
-export { default as computeQuantAxes } from "./axis_utils/compute_quant_axes";
-export { default as computeCategoricalAxes } from "./axis_utils/compute_categorical_axes";
-export { default as computeTimeAxes } from "./axis_utils/compute_time_axes";
-export { default as DataFrame } from "./DataFrame/DataFrame";
-export { PivotFramePreindexed } from "./DataFrame/PivotFramePreindexed";
-export { PivotFrame } from "./DataFrame/PivotFrame";
-export * from "./Grid/PivotGrid";
-export * from "./Grid/TableGrid";
-
-// Type exports
-export * from "./axis_utils/typings";
-export { AxesData, LegendDatum } from "./Chart/typings";
-export * from "./DataFrame/types";
+export { Axis } from "./Axis";
+export { Bars } from "./Bars";
+export { Chart } from "./Chart";
+export * from "./scale";
---
+++ packages/visualizations/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  resolve: {
+    alias: {
+      "@operational/frame": require("path").join(__dirname, "../frame"),
+    },
+  },
+};
