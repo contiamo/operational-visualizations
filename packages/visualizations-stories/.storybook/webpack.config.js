f1a69a9a628c10326ab8293a77cf2f44d23b3a28
stereobooster <stereobooster@gmail.com>
2019-05-31 17:34:28 +0200

    Trying to improve DX

--- packages/visualizations-stories/.storybook/webpack.config.js
+++ packages/visualizations-stories/.storybook/webpack.config.js
@@ -1,19 +1,19 @@
-const path = require("path");
-
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    include: path.resolve(__dirname, "../src"),
-    loaders: [
+    use: [
       {
-        loader: require.resolve("@storybook/addon-storysource/loader"),
-        options: { parser: "typescript" },
+        loader: require.resolve("awesome-typescript-loader"),
+        options: {
+          reportFiles: ["../**/src/**/*.{ts,tsx}"],
+        },
       },
-      require.resolve("ts-loader"),
+      // Optional
+      {
+        loader: require.resolve("react-docgen-typescript-loader"),
+      },
     ],
   });
-
   config.resolve.extensions.push(".ts", ".tsx");
-
   return config;
 };
