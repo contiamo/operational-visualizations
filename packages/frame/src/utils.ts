17b13037995db413b8def75f64d72222a77e8684
Imogen Mason <imogen@contiamo.com>
2019-07-19 08:54:37 +0200

    Fix isCursor util method. (#102)

--- packages/frame/src/utils.ts
+++ packages/frame/src/utils.ts
@@ -1,4 +1,4 @@
 import { ColumnCursor } from "./types";
 
 export const isCursor = (x: any): x is ColumnCursor<any> =>
-  !!x.index && x instanceof Function;
+  (!!x.index || x.index === 0) && x instanceof Function;
