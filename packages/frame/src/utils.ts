1be74db1d5920d30fa4b8723d57fe3d3cdc4cbbb
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-07-30 13:18:37 +0200

    Fix stackRowBy method.

--- packages/frame/src/utils.ts
+++ packages/frame/src/utils.ts
@@ -4,13 +4,10 @@ export const isCursor = (x: any): x is ColumnCursor<any> =>
   (!!x.index || x.index === 0) && x instanceof Function;
 
 export const stackRowBy = <Name extends string>(categorical: ColumnCursor<Name>, metric: ColumnCursor<Name>) => {
-  let value = 0;
-  let prevRow: RawRow;
+  let prev: Record<string, number> = {};
   return (row: RawRow) => {
-    value = prevRow && categorical(prevRow) === categorical(row)
-      ? value + metric(prevRow)
-      : 0;
-    prevRow = row;
+    const value = prev[categorical(row)] || 0;
+    prev[categorical(row)] = value + metric(row);
     return value
   }
-}
+};
