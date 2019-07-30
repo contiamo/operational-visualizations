1ae2fd084d58881648e3dcdf3fb1f15cd55eef09
stereobooster <stereobooster@gmail.com>
2019-07-30 19:50:17 +0200

    Calculate total

--- packages/frame/src/stats.ts
+++ packages/frame/src/stats.ts
@@ -2,6 +2,7 @@ import { IterableFrame, ColumnCursor } from "./types";
 
 interface StatsCacheItem {
   max?: number;
+  total?: number;
   unique?: string[];
 }
 
@@ -21,6 +22,7 @@ const getStatsCacheItem = <Name extends string>(
   return cacheEntry[column.index];
 };
 
+// rename to max
 export const maxValue = <Name extends string>(frame: IterableFrame<Name>, column: ColumnCursor<Name>): number => {
   const cacheItem = getStatsCacheItem(frame, column);
   if (cacheItem.max === undefined) {
@@ -42,10 +44,21 @@ export const maxValue = <Name extends string>(frame: IterableFrame<Name>, column
   return cacheItem.max!;
 };
 
-export const uniqueValues = <Name extends string>(
-  frame: IterableFrame<Name>,
-  column: ColumnCursor<Name>,
-): string[] => {
+export const total = <Name extends string>(frame: IterableFrame<Name>, column: ColumnCursor<Name>): number => {
+  const cacheItem = getStatsCacheItem(frame, column);
+  if (cacheItem.total === undefined) {
+    // https://github.com/contiamo/operational-visualizations/issues/72
+    let total: number | undefined = undefined;
+    frame.mapRows(row => {
+      total = total === undefined ? row[column.index] : total + row[column.index];
+    });
+    cacheItem.total = total!;
+  }
+  return cacheItem.total!;
+};
+
+// rename to unique
+export const uniqueValues = <Name extends string>(frame: IterableFrame<Name>, column: ColumnCursor<Name>): string[] => {
   const cacheItem = getStatsCacheItem(frame, column);
   if (cacheItem.unique === undefined) {
     const unique = new Set<string>();
@@ -61,16 +74,15 @@ export const uniqueValueCombinations = <Name extends string>(
   frame: IterableFrame<Name>,
   columns: Array<ColumnCursor<Name>>,
 ): Array<string[]> => {
-  const columnValues = columns.map(c => uniqueValues(frame, c))
+  const columnValues = columns.map(c => uniqueValues(frame, c));
 
   const combineValues = (i: number, values: string[]): Array<string[]> => {
     return i === columns.length - 1
       ? columnValues[i].map(val => [...values, val])
       : columnValues[i].reduce((arr: Array<string[]>, val) => {
-          return [...arr, ...combineValues(i + 1, [...values, val])]
-        }, [])
-  }
+          return [...arr, ...combineValues(i + 1, [...values, val])];
+        }, []);
+  };
 
-  return combineValues(0, [])
-}
-
+  return combineValues(0, []);
+};
