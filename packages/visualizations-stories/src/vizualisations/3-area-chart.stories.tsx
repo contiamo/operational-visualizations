2280b723fa7c08d4f3d96062c46835ecf456bcee
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-07-31 09:40:24 +0200

    Add stacked area charts.

--- packages/visualizations-stories/src/vizualisations/3-area-chart.stories.tsx
+++ packages/visualizations-stories/src/vizualisations/3-area-chart.stories.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
-import { DataFrame } from "@operational/frame";
+import {
+  DataFrame,
+  IterableFrame,
+  RawRow,
+  ColumnCursor,
+  stackRowBy
+} from "@operational/frame";
 import {
   Area,
   AxialChartProps,
@@ -53,7 +59,57 @@ const rawData = {
   ]
 };
 
+const rawDataStacked = {
+  columns: [
+    {
+      name: "Customer.Continent" as "Customer.Continent",
+      type: "string"
+    },
+    {
+      name: "Customer.Country" as "Customer.Country",
+      type: "string"
+    },
+    {
+      name: "Customer.City" as "Customer.City",
+      type: "string"
+    },
+    {
+      name: "Customer.AgeGroup" as "Customer.AgeGroup",
+      type: "string"
+    },
+    {
+      name: "Customer.Gender" as "Customer.Gender",
+      type: "string"
+    },
+    {
+      name: "sales" as "sales",
+      type: "number"
+    },
+    {
+      name: "revenue" as "revenue",
+      type: "number"
+    }
+  ],
+  rows: [
+    ["Europe", "Germany", "Berlin", "<50", "Female", 101, 10.2],
+    ["Europe", "Germany", "Dresden", "<50", "Female", 201, 20.2],
+    ["Europe", "Germany", "Hamburg", "<50", "Female", 301, 30.2],
+    ["Europe", "UK", "London", "<50", "Female", 401, 40.2],
+    ["Europe", "UK", "Edinburgh", "<50", "Female", 501, 50.2],
+    ["North America", "USA", "New York", "<50", "Female", 801, 80.2],
+    ["North America", "Canada", "Toronto", "<50", "Female", 801, 80.2],
+    ["Europe", "Germany", "Berlin", "<50", "Male", 101, 10.2],
+    ["Europe", "Germany", "Dresden", "<50", "Male", 201, 20.2],
+    ["Europe", "Germany", "Hamburg", "<50", "Male", 301, 30.2],
+    ["Europe", "UK", "London", "<50", "Male", 401, 40.2],
+    ["Europe", "UK", "Edinburgh", "<50", "Male", 501, 50.2],
+    ["North America", "USA", "New York", "<50", "Male", 801, 80.2],
+    ["North America", "Canada", "Toronto", "<50", "Male", 801, 80.2]
+  ]
+};
+
 const frame = new DataFrame(rawData.columns, rawData.rows);
+const stackedFrame = new DataFrame(rawDataStacked.columns, rawDataStacked.rows);
 
 interface LineChartProps<Name extends string> {
   width: number;
@@ -63,30 +119,74 @@ interface LineChartProps<Name extends string> {
   categorical: Name;
   metric: Name;
   metricDirection: AxialChartProps<string>["metricDirection"];
+  colorBy?: Name[];
+  stacked?: boolean;
 }
+
+const colorPalette = [
+  "#1499CE",
+  "#7C246F",
+  "#EAD63F",
+  "#343972",
+  "#ED5B17",
+  "#009691",
+  "#1D6199",
+  "#D31F1F",
+  "#AD329C",
+  "#006865"
+];
+
+export const joinArrayAsString = (array: string[]) => {
+  return (array || []).join("/");
+};
+
+export const getColorScale = (
+  data: IterableFrame<string>,
+  colorBy: Array<ColumnCursor<string>>
+) => {
+  if (colorBy.length === 0) {
+    return () => colorPalette[0];
+  }
+  const uniqueValues = data.uniqueValues(colorBy).map(joinArrayAsString);
+  return (row: RawRow) => {
+    const valuesString = joinArrayAsString(colorBy.map(cursor => cursor(row)));
+    const index = uniqueValues.indexOf(valuesString);
+    return colorPalette[index % colorPalette.length];
+  };
+};
 
 /**
  * Examples of how you can compose more complex charts out of 'atoms'
  */
-const LineChart = <Name extends string>({
+const AreaChart = <Name extends string>({
   width,
   height,
   margin,
   data,
   categorical,
   metric,
-  metricDirection
+  metricDirection,
+  colorBy,
+  stacked
 }: LineChartProps<Name>) => {
+  const categoricalCursor = data.getCursor(categorical);
+  const metricCursor = data.getCursor(metric);
   const categoricalScale = useScaleBand({
     frame: data,
-    column: data.getCursor(categorical),
-    range: metricDirection === "vertical" ? [0, width] : [0, height]
+    column: categoricalCursor,
+    range: metricDirection === "horizontal" ? [0, height] : [0, width]
   });
   const metricScale = useScaleLinear({
     frame: data,
-    column: data.getCursor(metric),
-    range: metricDirection === "vertical" ? [height, 0] : [0, width]
+    column: metricCursor,
+    categorical: stacked ? categoricalCursor : undefined,
+    range: metricDirection === "horizontal" ? [0, width] : [height, 0]
   });
+  const colorScale = getColorScale(
+    data,
+    (colorBy || []).map(c => data.getCursor(c))
+  );
+  const stackRow = stackRowBy(categoricalCursor, metricCursor);
 
   return (
     <Chart
@@ -95,15 +195,19 @@ const LineChart = <Name extends string>({
       margin={margin}
       style={{ background: "#fff" }}
     >
-      <Area
-        metricDirection={metricDirection}
-        data={data}
-        categorical={data.getCursor(categorical)}
-        metric={data.getCursor(metric)}
-        categoricalScale={categoricalScale}
-        metricScale={metricScale}
-        style={{ fill: "#1f78b4" }}
-      />
+      {data.groupBy(colorBy || []).map((grouped, key) => (
+        <Area
+          key={key}
+          metricDirection={metricDirection}
+          data={grouped}
+          categorical={data.getCursor(categorical)}
+          metric={data.getCursor(metric)}
+          categoricalScale={categoricalScale}
+          metricScale={metricScale}
+          stackRow={stacked ? stackRow : undefined}
+          style={{ fill: colorScale(grouped.row(0)), stroke: "#fff" }}
+        />
+      ))}
       <Axis
         scale={categoricalScale}
         position={metricDirection === "vertical" ? "bottom" : "left"}
@@ -122,7 +226,7 @@ storiesOf("@operational/visualizations/3. Area chart", module)
     const magicMargin = [5, 10, 20, 60] as ChartProps["margin"];
 
     return (
-      <LineChart
+      <AreaChart
         metric="sales"
         categorical="Customer.City"
         width={300}
@@ -137,7 +241,7 @@ storiesOf("@operational/visualizations/3. Area chart", module)
     // number of pixels picked manually to make sure that YAxis fits on the screen
     const magicMargin = 60;
     return (
-      <LineChart
+      <AreaChart
         metric="sales"
         categorical="Customer.City"
         width={300}
@@ -145,6 +249,40 @@ storiesOf("@operational/visualizations/3. Area chart", module)
         margin={magicMargin}
         data={frame}
         metricDirection="horizontal"
+      />
+    );
+  })
+  .add("stacked horizontal", () => {
+    // number of pixels picked manually to make sure that YAxis fits on the screen
+    const magicMargin = 60;
+    return (
+      <AreaChart
+        metric="sales"
+        categorical="Customer.City"
+        colorBy={["Customer.Gender"]}
+        stacked={true}
+        width={300}
+        height={300}
+        margin={magicMargin}
+        data={stackedFrame}
+        metricDirection="horizontal"
+      />
+    );
+  })
+  .add("stacked vertical", () => {
+    // number of pixels picked manually to make sure that YAxis fits on the screen
+    const magicMargin = 60;
+    return (
+      <AreaChart
+        metric="sales"
+        categorical="Customer.City"
+        colorBy={["Customer.Gender"]}
+        stacked={true}
+        width={300}
+        height={300}
+        margin={magicMargin}
+        data={stackedFrame}
+        metricDirection="vertical"
       />
     );
   });
--- packages/visualizations/src/Area.tsx
+++ packages/visualizations/src/Area.tsx
@@ -5,31 +5,37 @@ import { LinearAxialChart } from "./types";
 
 export const Area: LinearAxialChart<string> = React.memo(props => {
   const defaultTransform = useChartTransform();
-
-  const { metricDirection, data, transform, metric, categorical, metricScale, categoricalScale, style } = props;
+  const { metricDirection, data, transform, metric, categorical, metricScale, categoricalScale, stackRow, style } = props;
 
   // The categorical scale must be a band scale for composability with bar charts.
   // Half of the tick width must be added to align with the ticks.
   const categoricalTickWidth = categoricalScale.bandwidth();
+
   const pathData = data.mapRows(row => {
-    const categoricalValue = categoricalTickWidth / 2 + (categoricalScale(categorical(row)) as number);
-    const metricValue = metricScale(metric(row));
-    return (metricDirection === "vertical" ? [categoricalValue, metricValue] : [metricValue, categoricalValue]) as [
-      number,
-      number
-    ];
+    const rowBaseline = stackRow ? stackRow(row) : 0;
+    return {
+      c: categoricalTickWidth / 2 + (categoricalScale(categorical(row)) || 0),
+      m0: metricScale(rowBaseline),
+      m1: metricScale(metric(row) + rowBaseline)
+    };
   });
 
   const path =
-    area()
-      .x0(d => (metricDirection === "vertical" ? d[0] : metricScale.range()[0]))
-      .x1(d => d[0])
-      .y0(d => (metricDirection === "vertical" ? metricScale.range()[0] : d[1]))
-      .y1(d => d[1])(pathData) || "";
+    metricDirection === "vertical"
+      ? area<{ c: number; m0: number; m1: number }>()
+          .x0(d => d.c)
+          .x1(d => d.c)
+          .y0(d => d.m0)
+          .y1(d => d.m1)
+      : area<{ c: number; m0: number; m1: number }>()
+          .x0(d => d.m0)
+          .x1(d => d.m1)
+          .y0(d => d.c)
+          .y1(d => d.c);
 
   return (
     <g transform={transform || defaultTransform}>
-      <path d={path} style={style} />
+      <path d={path(pathData) || ""} style={style} />
     </g>
   );
 });
