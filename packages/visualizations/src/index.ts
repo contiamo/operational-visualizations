ba021351b0470d1343ac3ff4d53db626ba93899d
stereobooster <stereobooster@gmail.com>
2019-08-05 15:02:25 +0200

    Simplest scatter plot

--- packages/visualizations/src/index.ts
+++ packages/visualizations/src/index.ts
@@ -2,6 +2,7 @@ export { Axis } from "./Axis";
 export { Area } from "./Area";
 export { Bars } from "./Bars";
 export { Line } from "./Line";
+export { Dots } from "./Dots";
 export { Chart, ChartProps } from "./Chart";
 export { AxialChart, AxialChartProps } from "./types";
 export * from "./scale";
