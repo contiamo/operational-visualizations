0d60e4a768f8c0216214ef8562b8a54ea12e9c91
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-08-06 09:58:17 +0200

    Stacked areas.

--- packages/visualizations/src/Area.tsx
+++ packages/visualizations/src/Area.tsx
@@ -2,34 +2,51 @@ import { area } from "d3-shape";
 import React from "react";
 import { useChartTransform } from "./Chart";
 import { LinearAxialChart } from "./types";
+import { isFunction } from "./utils";
 
-export const Area: LinearAxialChart<string> = React.memo(props => {
+export const Area: LinearAxialChart<string> = props => {
   const defaultTransform = useChartTransform();
 
-  const { metricDirection, data, transform, metric, categorical, metricScale, categoricalScale, style } = props;
+  const { metricDirection, data, transform, metric, categorical, metricScale, categoricalScale, stack, style } = props;
 
   // The categorical scale must be a band scale for composability with bar charts.
   // Half of the tick width must be added to align with the ticks.
   const categoricalTickWidth = categoricalScale.bandwidth();
-  const pathData = data.mapRows(row => {
-    const categoricalValue = categoricalTickWidth / 2 + (categoricalScale(categorical(row)) as number);
-    const metricValue = metricScale(metric(row));
-    return (metricDirection === "vertical" ? [categoricalValue, metricValue] : [metricValue, categoricalValue]) as [
-      number,
-      number
-    ];
-  });
 
-  const path =
-    area()
-      .x0(d => (metricDirection === "vertical" ? d[0] : metricScale.range()[0]))
-      .x1(d => d[0])
-      .y0(d => (metricDirection === "vertical" ? metricScale.range()[0] : d[1]))
-      .y1(d => d[1])(pathData) || "";
+  const accumulatedCache: Record<string, number> = {};
+
+  const path = metricDirection === "vertical"
+    ? area<{ c: string; m0: number; m1: number }>()
+        .x(d => categoricalTickWidth / 2 + (categoricalScale(d.c) || 0))
+        .y0(d => metricScale(d.m0))
+        .y1(d => metricScale(d.m1))
+    : area<{ c: string; m0: number; m1: number }>()
+        .x0(d => metricScale(d.m0))
+        .x1(d => metricScale(d.m1))
+        .y(d => categoricalTickWidth / 2 + (categoricalScale(d.c) || 0));
 
   return (
     <g transform={transform || defaultTransform}>
-      <path d={path} style={style} />
+      {data.groupBy(stack || [])
+        .map((grouped, i) => {
+          const pathData = grouped.mapRows(row => {
+            const metricValue = metric(row);
+            const accumulatedValue = accumulatedCache[categorical(row)] || 0;
+            const newAccumulatedValue = accumulatedValue + metricValue
+            accumulatedCache[categorical(row)] = newAccumulatedValue
+            return {
+              c: categorical(row),
+              m0: accumulatedValue,
+              m1: newAccumulatedValue,
+            }
+          });
+
+          return <path
+            d={path(pathData) || ""}
+            style={isFunction(style) ? style(grouped.row(0), i) : style}
+          />
+        })
+      }
     </g>
   );
-});
+};
--- packages/visualizations/src/types.ts
+++ packages/visualizations/src/types.ts
@@ -9,22 +9,19 @@ export interface BaseAxialChartProps<Name extends string> {
   metricScale: ScaleLinear<any, any>;
   categoricalScale: ScaleBand<string>;
   transform?: React.SVGAttributes<SVGRectElement>["transform"];
-}
-
-export type DiscreteAxialChartProps<Name extends string> = BaseAxialChartProps<Name> & {
   style?:
     | React.SVGAttributes<SVGGElement>["style"]
     | ((row: RowCursor, i: number) => React.SVGAttributes<SVGGElement>["style"]);
-};
+}
 
 export type LinearAxialChartProps<Name extends string> = BaseAxialChartProps<Name> & {
-  style?: React.SVGAttributes<SVGGElement>["style"];
+  stack?: Array<ColumnCursor<Name>>
 };
 
-export type AxialChartProps<Name extends string> = DiscreteAxialChartProps<Name> | LinearAxialChartProps<Name>;
+export type AxialChartProps<Name extends string> = BaseAxialChartProps<Name> | LinearAxialChartProps<Name>;
 
 export type DiscreteAxialChart<Name extends string> = (
-  props: DiscreteAxialChartProps<Name>,
+  props: BaseAxialChartProps<Name>,
 ) => React.ReactElement | null;
 
 export type LinearAxialChart<Name extends string> = (props: LinearAxialChartProps<Name>) => React.ReactElement | null;
