172d97734319b38ef6813f5a83f9a965bd0b28b7
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-08-08 12:19:36 +0200

    Use d3 for color scale.

--- packages/visualizations/src/colors.ts
+++ packages/visualizations/src/colors.ts
@@ -1,44 +1,20 @@
 import { IterableFrame, RowCursor, ColumnCursor } from "@operational/frame";
 import theme from "./theme";
-import { joinArrayAsString, hashCursors } from "./utils";
+import { joinArrayAsString } from "./utils";
+import { scaleOrdinal } from "d3-scale";
 
 const defaultPalette = theme.palettes.qualitative.operational;
 
-type ColorCacheItem = Record<string, string>
-
-const colorCache = new WeakMap<IterableFrame<string>, Record<string, ColorCacheItem>>();
-
-const getColorCacheItem = (frame: IterableFrame<string>, key: string): ColorCacheItem => {
-  if (!colorCache.has(frame)) {
-    colorCache.set(frame, {})
-  }
-  const cacheEntry = colorCache.get(frame)!;
-  if (!cacheEntry[key]) {
-    cacheEntry[key] = {};
-  }
-  return cacheEntry[key];
-}
-
 export const getColorScale = (frame: IterableFrame<string>, colorBy: Array<ColumnCursor<string>>, palette: string[] = defaultPalette) => {
-  const cacheItem = getColorCacheItem(frame, hashCursors(colorBy));
   const uniqueValues = frame.uniqueValues(colorBy).map(joinArrayAsString);
+  const scale = scaleOrdinal().range(palette).domain(uniqueValues);
 
-  if (Object.entries(cacheItem).length === 0) {
-    if (colorBy.length === 0 || uniqueValues.length === 1) {
-      return () => palette[0];
-    }
-    uniqueValues.forEach(value => {
-        const index = uniqueValues.indexOf(value);
-        cacheItem[value] = palette[index % palette.length]
-    })
+  if (colorBy.length === 0 || uniqueValues.length === 1) {
+    return () => palette[0];
   }
 
   return (row: RowCursor) => {
     const valuesString = joinArrayAsString(colorBy.map(cursor => cursor(row)));
-    if (!cacheItem[valuesString]) {
-      const index = uniqueValues.indexOf(valuesString);
-      cacheItem[valuesString] = palette[index % palette.length]
-    }
-    return cacheItem[valuesString]
+    return scale(valuesString) || "#000"
   };
 };
--- packages/visualizations/src/utils.ts
+++ packages/visualizations/src/utils.ts
@@ -1,9 +1,4 @@
-import { ColumnCursor } from "@operational/frame";
-
 export const isFunction = (x: any): x is Function => x instanceof Function;
 
 export const joinArrayAsString = (array: any[]) =>
   (array || []).join(" / ");
-
-export const hashCursors = <Name extends string = string>(columns: Array<ColumnCursor<Name>>) =>
-  `${joinArrayAsString(columns.map(column => column.index))}`;
