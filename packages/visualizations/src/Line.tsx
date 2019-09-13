433311b4cc185d80fcdd30181999b01129452fe7
stereobooster <stereobooster@gmail.com>
2019-09-13 14:12:55 +0200

    Refactor Line

--- packages/visualizations/src/Line.tsx
+++ packages/visualizations/src/Line.tsx
@@ -5,21 +5,9 @@ import { LinearAxialChart } from "./types";
 import { isFunction, fillHoles } from "./utils";
 import { Labels } from "./Labels";
 import { isScaleBand, isScaleContinious } from "./scale";
-import { IterableFrame, ColumnCursor } from "@operational/frame";
-import { ScaleBand, ScaleLinear } from "d3-scale";
-
-const useFillHoles = (
-  data: IterableFrame<string>,
-  x: ColumnCursor<string>,
-  y: ColumnCursor<string>,
-  xScale: ScaleBand<string> | ScaleLinear<number, number>,
-  yScale: ScaleBand<string> | ScaleLinear<number, number>,
-) => useMemo(() => fillHoles(data, x, y, xScale, yScale), [data, x, y, xScale, yScale]);
 
 export const Line: LinearAxialChart<string> = React.memo(
   ({ data, transform, x, y, xScale, yScale, showLabels, style }) => {
-    const defaultTransform = useChartTransform();
-
     if (isScaleBand(xScale) && isScaleContinious(yScale)) {
       // do nothing
     } else if (isScaleBand(yScale) && isScaleContinious(xScale)) {
@@ -28,36 +16,34 @@ export const Line: LinearAxialChart<string> = React.memo(
       throw new Error("Unsupported case of scales");
     }
 
-    const dataWithHoles = useFillHoles(data, x, y, xScale, yScale);
+    const defaultTransform = useChartTransform();
 
     const path = useMemo(() => {
-      const pathData = dataWithHoles.map(row => {
-        const xCoordinate = isScaleBand(xScale) ? xScale.bandwidth() / 2 + (xScale(x(row)) || 0) : xScale(x(row));
-        const yCoordinate = isScaleBand(yScale) ? yScale.bandwidth() / 2 + (yScale(y(row)) || 0) : yScale(y(row));
-        return { xCoordinate, yCoordinate };
-      });
-
+      const xShift = isScaleBand(xScale) ? xScale.bandwidth() / 2 : 0;
+      const yShift = isScaleBand(yScale) ? yScale.bandwidth() / 2 : 0;
+      const pathData = fillHoles(data, x, y, xScale, yScale).map(row => ({ x: xScale(x(row)), y: yScale(y(row)) }));
       return (
-        line<{ xCoordinate: number; yCoordinate: number }>()
-          .x(d => d.xCoordinate)
-          .y(d => d.yCoordinate)
-          .defined(d => d.xCoordinate !== undefined && d.yCoordinate !== undefined)(pathData) || ""
+        line<{ x?: number; y?: number }>()
+          .x(d => d.x! + xShift)
+          .y(d => d.y! + yShift)
+          .defined(d => d.x !== undefined && d.y !== undefined)(pathData) || ""
       );
-    }, [dataWithHoles, x, y, xScale, yScale]);
+    }, [data, x, y, xScale, yScale]);
 
-    const pathStyle = (isFunction(style) ? style(data.row(0), 0) : style) || {};
+    const pathStyle = useMemo(
+      () =>
+        ({
+          fill: "none",
+          strokeLinecap: "round",
+          ...(isFunction(style) ? style(data.row(0), 0) : style),
+        } as React.SVGAttributes<SVGPathElement>["style"]),
+      [style, data],
+    );
 
     return (
       <>
         <g transform={transform || defaultTransform}>
-          <path
-            d={path}
-            style={{
-              fill: "none",
-              strokeLinecap: "round",
-              ...pathStyle,
-            }}
-          />
+          <path d={path} style={pathStyle} />
         </g>
         {showLabels && <Labels data={data} transform={transform} x={x} y={y} yScale={yScale} xScale={xScale} />}
       </>
