5cf3927b342af7cf01331d19d9a04406fb472513
stereobooster <stereobooster@gmail.com>
2019-09-16 11:21:19 +0200

    Undo Area and Line change which touched missing data code

--- packages/visualizations/src/Area.tsx
+++ packages/visualizations/src/Area.tsx
@@ -1,95 +1,123 @@
 import { area, line } from "d3-shape";
-import React, { useMemo } from "react";
+import React from "react";
 import { useChartTransform } from "./Chart";
 import { LinearAxialChart } from "./types";
-import { isFunction, fillHoles } from "./utils";
+import { isFunction } from "./utils";
 import { baseStyle as baseLabelStyle, verticalStyle as verticalLabelStyle } from "./Labels";
+import { ColumnCursor } from "@operational/frame";
 import { isScaleBand, isScaleContinuous } from "./scale";
-import { GroupFrame, ColumnCursor } from "@operational/frame";
-import { ScaleBand, ScaleLinear } from "d3-scale";
-
-const useFillHoles = (
-  data: GroupFrame<string>,
-  x: ColumnCursor<string>,
-  y: ColumnCursor<string>,
-  xScale: ScaleBand<string> | ScaleLinear<number, number>,
-  yScale: ScaleBand<string> | ScaleLinear<number, number>,
-) =>
-  useMemo(
-    () =>
-      data.map(grouped => ({
-        data: fillHoles(grouped, x, y, xScale, yScale),
-        firstRow: grouped.row(0),
-      })),
-    [data, x, y, xScale, yScale],
-  );
+import { ScaleBand } from "d3-scale";
 
 const isDefined = (value: number | undefined) => value !== undefined;
 
 export const Area: LinearAxialChart<string> = ({ data, transform, x, y, xScale, yScale, stack, showLabels, style }) => {
   const defaultTransform = useChartTransform();
 
+  let categorical: ColumnCursor<string>;
+  let metric: ColumnCursor<string>;
+  let categoricalScale: ScaleBand<string>;
+  if (isScaleBand(xScale) && isScaleContinuous(yScale)) {
+    categorical = x;
+    metric = y;
+    categoricalScale = xScale;
+  } else if (isScaleBand(yScale) && isScaleContinuous(xScale)) {
+    categorical = y;
+    metric = x;
+    categoricalScale = yScale;
+  } else {
+    throw new Error("Unsupported case of scales");
+  }
+
+  // The categorical scale must be a band scale for composability with bar charts.
+  // Half of the tick width must be added to align with the ticks.
+  const categoricalTickWidth = categoricalScale.bandwidth();
+
+  const missingDatum = (tick: string) => {
+    const d = [];
+    d[categorical.index] = tick;
+    d[metric.index] = undefined;
+    return d;
+  };
+
+  const ticks = categoricalScale.domain();
+
   const accumulatedCache: Record<string, number> = {};
 
-  const dataWithMissing = useFillHoles(data.groupBy(stack || []), x, y, xScale, yScale);
+  // The area path function takes an array of datum objects (here, called `d` for consistency with d3 naming conventions)
+  // with the following properties:
+  // `c` is the categorical tick value
+  // `m0` and `m1` are the lower and upper metric values
+  const path = isScaleBand(xScale)
+    ? area<{ c: string; m0: number; m1: number }>()
+        .x(d => categoricalTickWidth / 2 + (xScale(d.c) || 0))
+        .y0(d => yScale(d.m0 as any) as number)
+        .y1(d => yScale(d.m1 as any) as number)
+    : area<{ c: string; m0: number; m1: number }>()
+        .x0(d => xScale(d.m0))
+        .x1(d => xScale(d.m1))
+        .y(d => categoricalTickWidth / 2 + (yScale(d.c as any) || 0));
 
-  if (isScaleBand(xScale) && isScaleContinuous(yScale)) {
-    // The categorical scale must be a band scale for composability with bar charts.
-    // Half of the tick width must be added to align with the ticks.
-    const categoricalTickWidth = xScale.bandwidth();
+  const strokePath = isScaleBand(xScale)
+    ? line<{ c: string; m0: number; m1: number }>()
+        .x(d => categoricalTickWidth / 2 + (xScale(d.c) || 0))
+        .y(d => yScale(d.m1 as any) as number)
+    : line<{ c: string; m0: number; m1: number }>()
+        .x(d => xScale(d.m1))
+        .y(d => categoricalTickWidth / 2 + (yScale(d.c as any) || 0));
 
-    // The area path function takes an array of datum objects (here, called `d` for consistency with d3 naming conventions)
-    // with the following properties:
-    // `c` is the categorical tick value
-    // `m0` and `m1` are the lower and upper metric values
-    const path = area<{ c: string; m0: number; m1: number }>()
-      .x(d => categoricalTickWidth / 2 + (xScale(d.c) || 0))
-      .y0(d => yScale(d.m0))
-      .y1(d => yScale(d.m1));
+  const stackedData = data.groupBy(stack || []).map(grouped => {
+    const rawPathData = grouped.mapRows(row => row);
 
-    const strokePath = line<{ c: string; m0: number; m1: number }>()
-      .x(d => categoricalTickWidth / 2 + (xScale(d.c) || 0))
-      .y(d => yScale(d.m1));
+    // Add missing data
+    const dataWithMissing = ticks
+      .map(tick => {
+        const datum = rawPathData.find(d => categorical(d) === tick);
+        return datum || missingDatum(tick);
+      })
+      .sort(d => ticks.indexOf(categorical(d)));
 
-    const stackedData = dataWithMissing.map(row => ({
-      data: row.data.map(row => {
-        const metricValue = y(row);
-        const accumulatedValue = accumulatedCache[x(row)] || 0;
-        accumulatedCache[x(row)] = accumulatedValue + (metricValue || 0);
+    // Stack
+    return {
+      data: dataWithMissing.map(row => {
+        const metricValue = metric(row);
+        const accumulatedValue = accumulatedCache[categorical(row)] || 0;
+        accumulatedCache[categorical(row)] = accumulatedValue + (metricValue || 0);
         return {
-          c: x(row),
+          c: categorical(row),
           m0: accumulatedValue,
           m1: isDefined(metricValue) ? accumulatedValue + metricValue : undefined,
         };
       }),
-      firstRow: row.firstRow,
-    }));
+      firstRow: grouped.row(0),
+    };
+  });
 
-    return (
-      <g transform={transform || defaultTransform}>
-        {/* Render area segments */}
-        {stackedData.map((stack, i) => (
-          <path
-            key={`Area-${i}`}
-            d={path.defined(d => isDefined(d.m1))(stack.data) || ""}
-            style={{
-              strokeLinecap: "round",
-              ...(isFunction(style) ? style(stack.firstRow, i) : style),
-            }}
-          />
-        ))}
-        {/* Render white lines between each area segment. This is done afterwards to ensure the lines are visible */}
-        {stackedData.map((stack, i) => (
-          <path
-            key={`Line-${i}`}
-            d={strokePath.defined(d => isDefined(d.m1))(stack.data) || ""}
-            style={{ stroke: "#fff", fill: "none" }}
-          />
-        ))}
-        {/* Render text labels. This is done at the end to ensure they are visible */}
-        {showLabels &&
-          stackedData.map((stack, i) =>
-            stack.data.map((d, j) => (
+  return (
+    <g transform={transform || defaultTransform}>
+      {/* Render area segments */}
+      {stackedData.map((stack, i) => (
+        <path
+          key={`Area-${i}`}
+          d={path.defined(d => isDefined(d.m1))(stack.data) || ""}
+          style={{
+            strokeLinecap: "round",
+            ...(isFunction(style) ? style(stack.firstRow, i) : style),
+          }}
+        />
+      ))}
+      {/* Render white lines between each area segment. This is done afterwards to ensure the lines are visible */}
+      {stackedData.map((stack, i) => (
+        <path
+          key={`Line-${i}`}
+          d={strokePath.defined(d => isDefined(d.m1))(stack.data) || ""}
+          style={{ stroke: "#fff", fill: "none" }}
+        />
+      ))}
+      {/* Render text labels. This is done at the end to ensure they are visible */}
+      {showLabels &&
+        stackedData.map((stack, i) =>
+          stack.data.map((d, j) =>
+            isScaleBand(xScale) ? (
               <text
                 key={`Label-${i}-${j}`}
                 x={(xScale(d.c) || 0) + categoricalTickWidth / 2}
@@ -99,61 +127,7 @@ export const Area: LinearAxialChart<string> = ({ data, transform, x, y, xScale, 
               >
                 {isDefined(d.m1) && isDefined(d.m0) ? d.m1 - d.m0 : ""}
               </text>
-            )),
-          )}
-      </g>
-    );
-  } else if (isScaleBand(yScale) && isScaleContinuous(xScale)) {
-    const categoricalTickWidth = yScale.bandwidth();
-
-    const path = area<{ c: string; m0: number; m1: number }>()
-      .x0(d => xScale(d.m0))
-      .x1(d => xScale(d.m1))
-      .y(d => categoricalTickWidth / 2 + (yScale(d.c) || 0));
-
-    const strokePath = line<{ c: string; m0: number; m1: number }>()
-      .x(d => xScale(d.m1))
-      .y(d => categoricalTickWidth / 2 + (yScale(d.c) || 0));
-
-    const stackedData = dataWithMissing.map(row => ({
-      data: row.data.map(row => {
-        const metricValue = x(row);
-        const accumulatedValue = accumulatedCache[y(row)] || 0;
-        accumulatedCache[y(row)] = accumulatedValue + (metricValue || 0);
-        return {
-          c: y(row),
-          m0: accumulatedValue,
-          m1: isDefined(metricValue) ? accumulatedValue + metricValue : undefined,
-        };
-      }),
-      firstRow: row.firstRow,
-    }));
-
-    return (
-      <g transform={transform || defaultTransform}>
-        {/* Render area segments */}
-        {stackedData.map((stack, i) => (
-          <path
-            key={`Area-${i}`}
-            d={path.defined(d => isDefined(d.m1))(stack.data) || ""}
-            style={{
-              strokeLinecap: "round",
-              ...(isFunction(style) ? style(stack.firstRow, i) : style),
-            }}
-          />
-        ))}
-        {/* Render white lines between each area segment. This is done afterwards to ensure the lines are visible */}
-        {stackedData.map((stack, i) => (
-          <path
-            key={`Line-${i}`}
-            d={strokePath.defined(d => isDefined(d.m1))(stack.data) || ""}
-            style={{ stroke: "#fff", fill: "none" }}
-          />
-        ))}
-        {/* Render text labels. This is done at the end to ensure they are visible */}
-        {showLabels &&
-          stackedData.map((stack, i) =>
-            stack.data.map((d, j) => (
+            ) : (
               <text
                 key={`Label-${i}-${j}`}
                 x={xScale(d.m1)}
@@ -164,11 +138,9 @@ export const Area: LinearAxialChart<string> = ({ data, transform, x, y, xScale, 
               >
                 {isDefined(d.m1) && isDefined(d.m0) ? d.m1 - d.m0 : ""}
               </text>
-            )),
-          )}
-      </g>
-    );
-  } else {
-    throw new Error("Unsupported case of scales");
-  }
+            ),
+          ),
+        )}
+    </g>
+  );
 };
--- packages/visualizations/src/Line.tsx
+++ packages/visualizations/src/Line.tsx
@@ -1,49 +1,82 @@
 import { line } from "d3-shape";
-import React, { useMemo } from "react";
+import React from "react";
 import { useChartTransform } from "./Chart";
 import { LinearAxialChart } from "./types";
-import { isFunction, fillHoles } from "./utils";
+import { isFunction } from "./utils";
 import { Labels } from "./Labels";
+import { ScaleBand, ScaleLinear } from "d3-scale";
+import { ColumnCursor } from "@operational/frame";
 import { isScaleBand, isScaleContinuous } from "./scale";
 
 export const Line: LinearAxialChart<string> = React.memo(
   ({ data, transform, x, y, xScale, yScale, showLabels, style }) => {
+    const defaultTransform = useChartTransform();
+
+    let categorical: ColumnCursor<string>;
+    let metric: ColumnCursor<string>;
+    let categoricalScale: ScaleBand<string>;
+    let metricScale: ScaleLinear<number, number>;
     if (isScaleBand(xScale) && isScaleContinuous(yScale)) {
-      // do nothing
+      categorical = x;
+      metric = y;
+      categoricalScale = xScale;
+      metricScale = yScale;
     } else if (isScaleBand(yScale) && isScaleContinuous(xScale)) {
-      // do nothing
+      categorical = y;
+      metric = x;
+      categoricalScale = yScale;
+      metricScale = xScale;
     } else {
       throw new Error("Unsupported case of scales");
     }
 
-    const defaultTransform = useChartTransform();
+    // The categorical scale must be a band scale for composability with bar charts.
+    // Half of the tick width must be added to align with the ticks.
+    const categoricalTickWidth = categoricalScale.bandwidth();
 
-    const path = useMemo(() => {
-      const xShift = isScaleBand(xScale) ? xScale.bandwidth() / 2 : 0;
-      const yShift = isScaleBand(yScale) ? yScale.bandwidth() / 2 : 0;
-      const pathData = fillHoles(data, x, y, xScale, yScale).map(row => ({ x: xScale(x(row)), y: yScale(y(row)) }));
-      return (
-        line<{ x?: number; y?: number }>()
-          .x(d => d.x! + xShift)
-          .y(d => d.y! + yShift)
-          .defined(d => d.x !== undefined && d.y !== undefined)(pathData) || ""
-      );
-    }, [data, x, y, xScale, yScale]);
+    const missingDatum = (tick: string) => {
+      const d = [];
+      d[categorical.index] = tick;
+      d[metric.index] = undefined;
+      return d;
+    };
 
-    const pathStyle = useMemo(
-      () =>
-        ({
-          fill: "none",
-          strokeLinecap: "round",
-          ...(isFunction(style) ? style(data.row(0), 0) : style),
-        } as React.SVGAttributes<SVGPathElement>["style"]),
-      [style, data],
-    );
+    const rawData = data.mapRows(row => row);
+
+    // Add missing data
+    const ticks = categoricalScale.domain();
+    const dataWithMissing = ticks.map(tick => {
+      const datum = rawData.find(d => categorical(d) === tick);
+      return datum || missingDatum(tick);
+    });
+
+    const pathData = dataWithMissing.map(row => {
+      const categoricalValue = categoricalTickWidth / 2 + (categoricalScale(categorical(row)) as number);
+      const metricValue = metricScale(metric(row));
+      return { m: metricValue, c: categoricalValue };
+    });
+
+    const isDefined = (value: number | undefined) => value !== undefined;
+
+    const path =
+      line<{ m: number; c: number }>()
+        .x(d => (isScaleBand(xScale) ? d.c : d.m))
+        .y(d => (isScaleBand(xScale) ? d.m : d.c))
+        .defined(d => isDefined(d.m))(pathData) || "";
+
+    const pathStyle = (isFunction(style) ? style(data.row(0), 0) : style) || {};
 
     return (
       <>
         <g transform={transform || defaultTransform}>
-          <path d={path} style={pathStyle} />
+          <path
+            d={path}
+            style={{
+              fill: "none",
+              strokeLinecap: "round",
+              ...pathStyle,
+            }}
+          />
         </g>
         {showLabels && <Labels data={data} transform={transform} x={x} y={y} yScale={yScale} xScale={xScale} />}
       </>
