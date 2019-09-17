0f631693839cd191783693f6d85162ce9ac8a27b
stereobooster <stereobooster@gmail.com>
2019-09-17 15:44:45 +0200

    feat(Axis): Limit max number of labels on the axis

--- packages/visualizations/src/Axis.tsx
+++ packages/visualizations/src/Axis.tsx
@@ -3,6 +3,7 @@ import { ScaleBand, ScaleLinear } from "d3-scale";
 import { select } from "d3-selection";
 import React, { useEffect, useRef } from "react";
 import { useAxisTransform } from "./Chart";
+import { isScaleContinuous } from "./scale";
 
 export interface AxisProps {
   /** see  https://github.com/d3/d3-scale */
@@ -11,28 +12,32 @@ export interface AxisProps {
   position: "left" | "right" | "bottom" | "top";
   /** see https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/transform */
   transform?: string;
+  maxNumberOfTicks?: number;
 }
 
-export const Axis: React.FC<AxisProps> = React.memo(({ scale, transform, position }) => {
+export const Axis: React.FC<AxisProps> = React.memo(({ scale, transform, position, maxNumberOfTicks = 10 }) => {
   const defaultTransform = useAxisTransform(position!);
   const ref = useRef<SVGGElement>(null);
   useEffect(() => {
     if (ref.current) {
+      const ticks = (isScaleContinuous(scale) ? scale.ticks() : scale.domain()).length;
+      const tickFormat =
+        ticks > maxNumberOfTicks ? (d: any, i: number) => (i % maxNumberOfTicks === 0 ? d : null) : (d: any) => d;
       switch (position) {
         case "bottom":
-          select(ref.current).call(axisBottom(scale));
+          select(ref.current).call(axisBottom(scale).tickFormat(tickFormat));
           break;
         case "top":
-          select(ref.current).call(axisTop(scale));
+          select(ref.current).call(axisTop(scale).tickFormat(tickFormat));
           break;
         case "left":
-          select(ref.current).call(axisLeft(scale));
+          select(ref.current).call(axisLeft(scale).tickFormat(tickFormat));
           break;
         case "right":
-          select(ref.current).call(axisRight(scale));
+          select(ref.current).call(axisRight(scale).tickFormat(tickFormat));
           break;
       }
     }
-  }, [ref, scale, position]);
+  }, [ref, scale, position, maxNumberOfTicks]);
   return <g transform={transform || defaultTransform} ref={ref} />;
 });
