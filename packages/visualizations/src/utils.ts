1c155418be81b542959c98db002faaf3574ddad9
Imogen Fletcher <imogen.n.fletcher@gmail.com>
2019-09-18 10:57:42 +0200

    Improve number formatting.

--- packages/visualizations/src/utils.ts
+++ packages/visualizations/src/utils.ts
@@ -4,4 +4,15 @@ export const isFunction = (x: any): x is Function => x instanceof Function;
 
 export const joinArrayAsString = (array: any[]) => (array || []).join(" / ");
 
-export const numberFormatter = d3Format(",.2f");
+export const numberFormatter = (val: number) => {
+  if (val > 100) {
+    return d3Format(",.0f");
+  }
+  if (val > 10) {
+    return d3Format(",.1f");
+  }
+  if (val > 0) {
+    return d3Format(",.2f");
+  }
+  return d3Format(".,3f");
+};
